dd7fe7c69d60ec5aa955ebd15ee1daee7b323cbe
levisen <jkv@levisen.eu>
2022-09-19 09:15:41 +0200

    added outputs

--- code/src/assignment1/ProducerConsumer/producer.cpp
+++ code/src/assignment1/ProducerConsumer/producer.cpp
@@ -6,6 +6,7 @@ void Producer::produce(void) {
 		wait((rand()%(10-2 + 1) + 2), SC_MS);
 		package = new TCPHeader();
 		package_number++;
+		package->DestinationPort = rand() % 2 + 1;
 		package->SequenceNumber = package_number;
 		for (int i = 0; i < out.size(); i++)
 		{
