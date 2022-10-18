b257cb9c5ac11f80543c004ce7d66b6d7c99d60a
Jakob Levisen <jkv@levisen.eu>
2022-10-18 16:57:20 +0200

    Updates from Overleaf

---
+++ code/src/assignment2/advios_driver.h
@@ -0,0 +1,31 @@
+#ifndef _IOS_DRIVER
+#define _IOS_DRIVER
+
+#include <systemc.h>
+
+SC_MODULE(advios_driver) {
+
+	//Ports
+	sc_in <bool> clk;
+	sc_out <bool> reset;
+
+	sc_out<sc_uint<4> > ctrl;
+	sc_out<sc_uint<4> > outSwitch;
+	sc_in<sc_uint<4> > inLeds;
+
+	int retval;
+
+	//Process Declaration
+	void test();
+
+	//Constructor
+	SC_CTOR(advios_driver) : retval(-1) {
+
+		//Process Registration
+		SC_CTHREAD(test, clk.pos());
+		reset_signal_is(reset, true);
+		//sensitive << clk.pos();
+	}
+};
+
+#endif
